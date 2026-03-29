16b949f1450beac2aa4ca4e45a7529ade6388a08
Don-topo <rubengarridovaldepenas@gmail.com>
2026-03-29 20:28:08 +0200

    Keep working on attack action

---
+++ Assets/Scripts/Actions/AttackAction.cs
@@ -0,0 +1,29 @@
+using System.Data;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Attack", menuName = "Units/Actions/Attack", order = 99)]
+public class AttackAction : BaseAction
+{
+    [SerializeField] MoveAction moveAction;
+
+    public override bool CanExecute(ActionInfo actionInfo)
+    {
+        return actionInfo.Action is IAttackable && actionInfo.Hit.collider != null;
+    }
+
+    public override void Execute(ActionInfo actionInfo)
+    {
+        IAttacker attacker = actionInfo.Action as IAttacker;
+        if (attacker == null) return;
+        IAttackable attackable = actionInfo.Hit.collider.GetComponent<IAttackable>();
+        if (attackable == null) return;
+        if(moveAction != null)
+        {
+            //attacker.Attack(moveAction);
+        }
+        else
+        {
+            attacker.Attack(attackable);
+        }        
+    }
+}
