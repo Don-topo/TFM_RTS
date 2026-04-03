2be4d8cd467a599588b252bc6ad44eb6d1b4d9f1
Don-topo <rubengarridovaldepenas@gmail.com>
2026-04-03 15:41:29 +0200

    Implemented generation resource

--- Assets/Scripts/UI/UIResources.cs
+++ Assets/Scripts/UI/UIResources.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil;
 using TMPro;
 using UnityEngine;
 
@@ -11,12 +12,21 @@ public class UIResources : MonoBehaviour
     [SerializeField] private TextMeshProUGUI electricityText;
     [SerializeField] private TextMeshProUGUI populationText;
     // Resources types
-    [SerializeField] private SO_Resource food;
-    [SerializeField] private SO_Resource wood;
-    // Resources values
-
+    [SerializeField] private SO_Resource so_food;
+    [SerializeField] private SO_Resource so_wood;
+    [SerializeField] private SO_Resource so_stone;
+    [SerializeField] private SO_Resource so_iron;
+    [SerializeField] private SO_Resource so_electricity;
     // Events
-    ResourceEvent resourceEvent;
+    [SerializeField] private ResourceEvent resourceEvent;
+    // Resources values
+    public static int Food { get; private set; } = 0;
+    public static int Wood { get; private set;} = 0;
+    public static int Stone { get; private set; } = 0;
+    public static int Iron { get; private set; } = 0;
+    public static int Electricity { get; private set; } = 0;
+
+
 
     private void Awake()
     {
@@ -30,6 +40,25 @@ public class UIResources : MonoBehaviour
 
     private void UpdateResource(SO_Resource resource)
     {
-
+        if(resource == so_food)
+        {
+            Food += resource.ObtainedAmount;
+            foodText.SetText(Food.ToString());
+        }
+        else if(resource == so_wood)
+        {
+            Wood += resource.ObtainedAmount;
+            woodText.SetText(Wood.ToString());
+        }
+        else if(resource == so_iron)
+        {
+            Iron += resource.ObtainedAmount;
+            ironText.SetText(Iron.ToString());
+        }
+        else if(resource == so_electricity)
+        {
+            Electricity += resource.ObtainedAmount;
+            electricityText.SetText(Electricity.ToString());
+        }
     }
 }
